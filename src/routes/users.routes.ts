ba58a5d429caa0d3e78785bdb4d29f8188762dae
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-03 12:11:25 +0000

    inlcuded the route for create user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { welcome } from "../controllers/user.controller";
+import { createUser, welcome } from "../controllers/user.controller";
 
 const router = express.Router();
 
-router.get('/all', welcome)
+router.get('/all', welcome);
+router.post('/create', createUser);
 
 
 export default router;
