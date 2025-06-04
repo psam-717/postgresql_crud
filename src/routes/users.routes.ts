d7b721c8a1526be032e8831a153b8475fba5d207
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-04 00:15:07 +0000

    specified routes for login and included the middleware to validate req body

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { createUser, welcome } from "../controllers/user.controller";
+import { createUser, login, welcome } from "../controllers/user.controller";
+import { validateBody } from "../middlewares/validate.middleware";
+import { createUserSchema, loginSchema } from "../validators/user.validation";
 
 const router = express.Router();
 
 router.get('/all', welcome);
-router.post('/create', createUser);
+router.post('/create', validateBody(createUserSchema) ,createUser);
+router.post('/login', validateBody(loginSchema), login)
 
 
 export default router;
