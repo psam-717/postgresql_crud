9396d94136d3826619a7b636c0c90b6b774d9ca5
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-04 00:13:24 +0000

    modified the createUser functionality and added the login functionality

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from "../generated/prisma";
 import { Request, Response } from "express"
 import bcrypt from 'bcrypt'
+import { createUserSchema, loginSchema } from "../validators/user.validation";
+import { ValidatedRequest } from "../middlewares/validate.middleware";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const prisma = new PrismaClient();
 
@@ -17,16 +23,29 @@ export const welcome = async (req: Request, res: Response): Promise<void> => {
     }
 }
 
-export const createUser = async (req: Request, res: Response): Promise<void> => {
+export const createUser = async (req: ValidatedRequest<typeof createUserSchema>, res: Response): Promise<void> => {
     try {
         const {firstName, lastName, email, password, phone, location} = req.body;
-
-        if(!firstName || !lastName || !email || !password || !phone || !location){
-            res.status(400).json({message: 'All fields should be provided'})
-        }
         
-        const hashedPassword = await bcrypt.hash(password,10);
+        //make sure user does not exist
+        const existingUser = await prisma.customer.findFirst({
+            where: {
+                OR: [{email}, {phone}]
+            }
+        });
 
+        if(existingUser){
+            if(existingUser.email === email){
+                res.status(400).json({error: 'User with this email already exists'});
+                return;
+            }
+            if(existingUser.phone === phone){
+                res.status(400).json({error: 'User with phone number already exists'});
+                return;
+            }
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
         //create user
         const newUser = await prisma.customer.create({
            data: {
@@ -48,3 +67,49 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
         return;
     }
 }
+
+
+
+
+export const login = async(req: ValidatedRequest<typeof loginSchema>, res: Response):Promise<void> => {
+    try {
+        const {email, password} = req.body;
+
+        const exitingUser = await prisma.customer.findUnique({
+            where: {
+                email: email
+            }
+        })
+
+        if(!exitingUser){
+            res.status(404).json({error: 'User not found'});
+            return;
+        }
+
+        //compare password
+        const isMatch = await bcrypt.compare(password, exitingUser.password);
+        if(!isMatch){
+            res.status(403).json({error: 'Password is incorrect'});
+            return;
+        }
+
+        const token = jwt.sign(
+            {id: exitingUser.id, email: exitingUser.email},
+            process.env.JWT_SECRET as string,
+            {expiresIn: '1h'}
+        );
+
+        const {password: _, ...userWithoutPassword} = exitingUser;
+        res.status(200).json({
+            message: 'Logged in successfully',
+            token,
+            user: userWithoutPassword
+            
+        })
+        
+    } catch (error) {
+        console.log('Error caused by ', (error as Error).stack);
+        res.status(500).json({message: 'Internal server error while logging in'});
+        return
+    }
+} 
