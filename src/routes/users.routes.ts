881e0f5a9218d042eef0353e7e43fe99922242a7
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-06-04 01:08:42 +0000

    specified route for retrieving user data and inlcuded middleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,13 +1,15 @@
 import express from "express";
-import { createUser, login, welcome } from "../controllers/user.controller";
+import { createUser, login, profileData, welcome } from "../controllers/user.controller";
 import { validateBody } from "../middlewares/validate.middleware";
 import { createUserSchema, loginSchema } from "../validators/user.validation";
+import { authenticateToken } from "../middlewares/authorization";
 
 const router = express.Router();
 
 router.get('/all', welcome);
 router.post('/create', validateBody(createUserSchema) ,createUser);
-router.post('/login', validateBody(loginSchema), login)
+router.post('/login', validateBody(loginSchema), login);
+router.get('/me', authenticateToken, profileData);
 
 
 export default router;
